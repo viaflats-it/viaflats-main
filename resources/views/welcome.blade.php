6c21127250092f1c3ba1157d5feebb227150141e
Fabien <grd.fabien@gmail.com>
2016-10-26 11:26:17 +0200

    update since i was sick

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,21 +10,55 @@
     <div class="home-container">
       <div class="header-container">
         <div class="logo-container">
-
+          <img class="logo-viaflats" src="images/logos/logo-viaflats.png" alt="logo" />
         </div>
         <div class="nav-container">
-          <ul>
-            <li>Values</li>
-            <li>Viaflats</li>
-            <li>Support</li>
-            <li>Cities</li>
+          <ul class="nav-info">
+            <li><h3>Values</h3></li>
+            <li><h3>Viaflats</h3></li>
+            <li><h3>Support</h3></li>
+            <li><h3>Cities</h3></li>
           </ul>
         </div>
-        <div class="login-container">
-
+        <div class="left-container">
+          <ul class="burger-container">
+            <li>
+               <span class="line-1"></span>
+               <span class="line-2"></span>
+               <span class="line-3"></span>
+            </li>
+          </ul>
+        </div>
+        <div class="right-container">
+          <ul class="socials-container">
+            <li><img class="networks" src="images/networks/linkedin.png" alt="linkedin" /></li>
+            <li><img class="networks" src="images/networks/twitter.png" alt="twitter" /></li>
+            <li><img class="networks" src="images/networks/facebook.png" alt="facebook" /></li>
+            <li><img class="networks" src="images/networks/instagram.png" alt="instagram" /></li>
+          </ul>
+          <ul class="login-container">
+            <li class="login">Login</li>
+            <li class="signup">Signup</li>
+          </ul>
         </div>
       </div>
       <div class="search-container">
+        <input type="text" name="location" class="location" placeholder="Where do you want to go ?">
+        <input type="text" name="check-in" class="check-in" placeholder="Check in">
+        <input type="text" name="check-out" class="check-out" placeholder="Check out">
+        <input type="submit" name="search" class="search" value="Search">
+      </div>
+      <div class="background-container">
+        <video preload="preload" class="video" autoplay="autoplay" loop="loop"><source src="images/videos/background.mp4" type="video/mp4"></video>
+      </div>
+      <div class="background-color"></div>
+      <div class="label-container-home">
+        <h1>FIND.</h1>
+        <h1>BOOK.</h1>
+        <h1>ENJOY.</h1>
+      </div>
+      <div class="buildings-container">
+        <img class="svg" src="images/svgs/building1.svg" alt="Kiwi standing on oval">
       </div>
     </div>
   </body>
